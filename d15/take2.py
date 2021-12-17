540f379bf4b01cbe179da2b1446303e6ec9db045
David McFadzean <davidmc@gmail.com>
2021-12-17 08:40:36 -0500

    no joy

--- d15/take2.py
+++ d15/take2.py
@@ -1,8 +1,12 @@
+import numpy as np
 from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import shortest_path
 
-input = """
-1163751742
+input = """392
+721
+413"""
+
+input = """1163751742
 1381373672
 2136511328
 3694931569
@@ -13,17 +17,72 @@ input = """
 1293138521
 2311944581"""
 
-def makeGraph(input):
-    graph = []
+input = open('data', 'r').read()
+
+def makeGrid(input):
+    grid = []
     lines = input.split('\n')
     for line in lines:
-        graph.append([int(n) for n in line])
+        grid.append([int(n) for n in line])
+    return grid
+
+def makeGraph(grid):    
+    w = len(grid[0])
+    h = len(grid)
+
+    sz = w * h
+    print(f"w={w} h={h} sz={sz}")
+    graph = np.zeros((sz,sz))
+
+    # print(graph)
+
+    for i in range(h):
+        for j in range(1, w):
+            print(i, j, grid[i][j])
+            n1 = i*w + j
+            n2 = i*w + j - 1
+            graph[n1, n2] = grid[i][j]
+
+    for j in range(w):
+        for i in range(1, h):
+            print(i, j, grid[i][j])
+            n1 = (i-1)*w + j
+            n2 = i*w + j
+            graph[n1, n2] = grid[i][j]
+
     return csr_matrix(graph)
 
-graph = makeGraph(input)
+grid = makeGrid(input)
+print(grid)
+graph = makeGraph(grid)
 print(graph)
 
-dist_matrix, predecessors = shortest_path(csgraph=graph, directed=False, return_predecessors=True)
+dist_matrix, predecessors = shortest_path(csgraph=graph, method='BF', directed=False, indices=0, return_predecessors=True)
 
 print(dist_matrix)
 print(predecessors)
+
+print(dist_matrix[-1])
+
+n = predecessors[-1]
+path = []
+ht = len(grid)
+width = len(grid[0])
+while n >= 0:
+    i = n%width
+    j = n//width
+    c = (j, i)
+    path.append(c)
+    #print(n, c)
+    n = predecessors[n]
+path.reverse()
+path.append((ht-1,width-1)) 
+print(path)
+print(len(path))  
+
+risk = 0
+for i, j in path[1:]:
+    risk += grid[i][j]
+    print((i, j), grid[i][j], risk)
+
+print(dist_matrix[-1])
